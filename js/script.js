1367db111e98deef824ff9f90a8cf35010318066
Evgeniy <example@gmail.com>
2021-03-27 00:17:03 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// const number = 123;
+// const str = `Jenya`;
+// const bool = false;
+
+// console.log(typeof number);
+// console.log(typeof str);
+// console.log(typeof bool);
+
+// const result = '123' + '123';
+// console.log(result);
+
+// let no;
+// console.log(no);
+
+// const re = 123 / 'asd';
+// console.log(re);
+
+
+// alert('Hello');
+
+// const here = confirm('Вам есть 18 лет?');
+// console.log(here);
+
+// const answer = +prompt('Сколько вам лет?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
