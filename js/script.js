212681086e96a885f39c367deb295714deac4f7b
Evgeniy <example@gmail.com>
2021-03-28 12:58:36 +0500

    if else, switch

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 /* Задание на урок:
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос: 
 'Сколько фильмов вы уже посмотрели?'
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
@@ -23,23 +23,54 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-let a = prompt('Один из проследних просмотренных фильмов?','');
-let b = prompt('На сколько оцените его?','');
-let c = prompt('Один из проследних просмотренных фильмов?','');
-let d = prompt('На сколько оцените его?','');
+// let a = prompt('Один из последних просмотренных фильмов?', '');
+// let b = prompt('На сколько оцените его?', '');
+// let c = prompt('Один из последних просмотренных фильмов?', '');
+// let d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.moveis[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// Задание выполнено!
+
+// Условия
+
+let num = 50;
+
+if (num < 49) {
+    console.log('49 не равно 50');
+} else if (num > 51) {
+    console.log('51 не равно 50');
+} else {
+    console.log('50 равно 50');
+}
+
+switch (num) {
+    case 49:
+        console.log('Jenya');
+        break;
+    case 55:
+        console.log('Evgeniy');
+        break;
+    case 56: 
+        console.log('1998');
+        break;
+    default: 
+        console.log('Ничего не верно');
+        break;
+}
+
+
 
